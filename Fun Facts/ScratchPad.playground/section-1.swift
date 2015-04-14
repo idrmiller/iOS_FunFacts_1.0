41d5b8059aa233773694a87f55079e2cf0e62a06
idrmiller <idrmiller360@gmail.com>
2015-04-13 22:17:05 -0400

    Master

---
+++ Fun Facts/ScratchPad.playground/section-1.swift
@@ -0,0 +1,90 @@
+// Playground - noun: a place where people can play
+
+import UIKit
+
+var str = "Hello, playground"
+
+// Swift Recap
+
+var someVariable = "aVariable"
+let someConstant = 20
+
+someVariable = "somethingNew"
+
+
+
+//var fruitsArray: [String] = ["apples"]
+
+var fruitsArray = ["apples"]
+fruitsArray.append("bananas")
+
+fruitsArray
+
+
+//Optional
+
+var optionalString: String? = "hello"
+optionalString = nil
+
+//Random Number Generator
+
+////    -> The upper bound of this coude is 10
+var randomNumber0 = arc4random_uniform(10)
+Int(randomNumber0)
+
+var randomNumber = Int(arc4random_uniform(10))
+
+arc4random()
+arc4random_uniform(5)
+
+//  UIColor
+
+var redColor = UIColor(red: 223/255.0, green: 86/255.0, blue: 94/255.0, alpha: 1.0)
+
+let colorsArray = [redColor]
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
--- Fun Facts/ViewController.swift
+++ Fun Facts/ViewController.swift
@@ -9,17 +9,38 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    //  This is the target of our action method in @IBAction apart of the (Target - Action)
+    
+    @IBOutlet weak var funFactLabel: UILabel!
+    @IBOutlet weak var funFactButton: UIButton!
+  
+    
+    let factBook = FactBook()
+    let colorWheel = ColorWheel()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        funFactLabel.text = factBook.randomFact()
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
+    
+    // This is the action method that will pass the information to our target method to display on the screen @IBOutlet and aprt of the (Target - Action)
+    
+    @IBAction func showFunFact() {
+        var randomColor = colorWheel.randomColor()
+        funFactLabel.text = factBook.randomFact()
+        view.backgroundColor = randomColor
+        funFactButton.tintColor = randomColor
+        
+        
+    }
 
 }
 
